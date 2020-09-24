c5628b90ecbcffe01dc7201ba07f86adc711f27c
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-24 02:51:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,15 @@
-require 'pry'
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key, value), final_arr|
+  final_results = data.each_with_object({}) do |(key, value), final_arr|
     value.each do |inner_key, names|
       names.each do |name|
+        if !final_arr[name]
+          final_arr[name] = {}
+        end
+        if !final_arr[name][key]
+          !final_arr[name][key] = []
+        end
+        final_arr[name][key].push(inner_key)
       end
     end
-  final_arr
   end
 end
